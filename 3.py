be93ccf47ed6e2c1e2ad34309d33c74af0cb323f
ssrajput100 <103556510+ssrajput100@users.noreply.github.com>
2022-09-19 10:46:14 +0530

    Adding a colour vdo beside the gray one

--- 3.py
+++ 3.py
@@ -7,7 +7,8 @@ while(True):
     ret, frame = capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
  
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
+    cv2.imshow('gray',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
